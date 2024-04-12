cc191c04bbdf83e3c43fc781c0f5bded033ccd71
Rmhibbert <127159806+Rmhibbert@users.noreply.github.com>
2024-04-12 13:56:51 +1200

    Add files via upload

---
+++ 01-introduction-to-unity-scripting/01-starter-code/Assets/RW/Scripts/sheepManage.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sheepManage : MonoBehaviour
+{
+
+    public GameObject sheepPrefab;
+    public Transform sheepSpawnPoint;
+    public float interval;
+    public float timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        spawner();
+    }
+
+    void spawner()
+    {
+        timer -= Time.deltaTime;
+        if (timer > interval)
+        {
+            sheepJoin();
+            timer = interval;
+        }
+    }
+
+    private void sheepJoin()
+    {
+        Instantiate(sheepPrefab, sheepSpawnPoint.position, Quaternion.identity);
+    }
+}
---
+++ 01-introduction-to-unity-scripting/01-starter-code/Assets/RW/Scripts/sheepRun.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sheepRun : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public float speed;
+    public GameObject hay;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(transform.forward * -speed * Time.deltaTime);
+    }
+    private void OnTriggerEnter(Collider hay)
+    {
+        Destroy(gameObject);
+    }
+
+
+}
